4a19f2baaa6c88718326cebae0a8e154338b04f0
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-02-22 18:01:39 +0100

    milestone 2

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const euroKm = 0.21;
 
 // Inserimento in una variabile del bottone genera
 const btnGenera = document.getElementById('btn-genera');
+const btnAnnulla = document.getElementById('btn-annulla');
+const bigliettoShow = document.getElementById('biglietto');
 
 // Funzione al click del bottone genera
 btnGenera.addEventListener(
@@ -12,6 +14,8 @@ btnGenera.addEventListener(
     function(){
         // Inserimento del gli input dell'utente in delle variabili
         let sconto = 0;
+        let tipoBiglietto = "Biglietto Standard";
+        let carrozzaRandom = Math.random(1,10);
         const km = Number(document.getElementById('distanza').value);
         const etaUtente = document.getElementById('eta').value;
         const nomeUtente = document.getElementById('name').value;
@@ -26,15 +30,33 @@ btnGenera.addEventListener(
         if(etaUtente === 'm'){
             prezzoBiglietto *= 0.8;
             sconto = 20;
+            tipoBiglietto = "Biglietto Young";
         }else if(etaUtente  === 'V'){
             prezzoBiglietto *= 0.6;
             sconto = 40;
+            tipoBiglietto = "Biglietto Vecio";
         }
         // Stampa a video del prezzo finale
         console.log(`Prezzo Scontato del ${sconto}%: ${prezzoBiglietto.toFixed(2)}€`);
+
+        // Comparsa del biglietto con le informazioni inserite dall'utente
+        bigliettoShow.classList.add('show');
+        document.querySelector('.name').innerHTML = nomeUtente;
+        document.getElementById('tipo-biglietto').innerHTML = tipoBiglietto;
+        document.getElementById('carrozza').innerHTML = Math.floor(carrozzaRandom);
+        document.getElementById('prezzo').innerHTML = prezzoBiglietto.toFixed(2);
+        bigliettoShow.classList.remove('hidden');
     }
 );
 
+// Scomparse del biglietto al click del pulsante annulla
+btnAnnulla.addEventListener(
+    'click',
+    function(){
+        bigliettoShow.classList.remove('show');
+        bigliettoShow.classList.add('hidden');
+    }
+)
 
 
 
