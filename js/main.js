0f8f76b2dc8224e82f5dde6dfc6ad9c328c2ad7a
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-02-22 17:05:36 +0100

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -3,19 +3,30 @@
 const euroKm = 0.21;
 
 const btnGenera = document.getElementById('btn-genera');
-let etaUtente = document.getElementById('eta').value;
-let km = Number(document.getElementById('distanza').value);
 
 btnGenera.addEventListener(
     'click',
     function(){
-        km = Number(document.getElementById('distanza').value);
-        etaUtente = document.getElementById('eta').value;
+        let sconto = 0;
+        const km = Number(document.getElementById('distanza').value);
+        const etaUtente = document.getElementById('eta').value;
+        const nomeUtente = document.getElementById('name').value;
+        console.log(nomeUtente);
         console.log({km});
         console.log({etaUtente});
+        let prezzoBiglietto = euroKm * km;
+        console.log(`${prezzoBiglietto.toFixed(2)}€`);
+        if(etaUtente === 'm'){
+            prezzoBiglietto *= 0.8;
+            sconto = 20;
+        }else if(etaUtente  === 'V'){
+            prezzoBiglietto *= 0.6;
+            sconto = 40;
+        }
+        console.log(`Prezzo Scontato del ${sconto}%: ${prezzoBiglietto.toFixed(2)}€`);
     }
 );
 
-let prezzoBiglietto = euroKm * km;
 
-console.log({prezzoBiglietto});
+
+
