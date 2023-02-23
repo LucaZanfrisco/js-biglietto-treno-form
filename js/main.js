33b492ebe9dc222da720085dbc24bdd367f28116
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-02-23 15:06:27 +0100

    fix bottoni , add controllo input, fix variabili

--- js/main.js
+++ js/main.js
@@ -6,51 +6,64 @@ const euroKm = 0.21;
 // Inserimento in una variabile del bottone genera
 const btnGenera = document.getElementById('btn-genera');
 const btnAnnulla = document.getElementById('btn-annulla');
+
+// variabile bilgietto
 const bigliettoShow = document.getElementById('biglietto');
+
+// Definizione e assegnamento degli elementi input 
+const km = document.getElementById('distanza');
+const etaUtente = document.getElementById('eta');
+const nomeUtente = document.getElementById('name');
 
 // Funzione al click del bottone genera
 btnGenera.addEventListener(
     'click',
     function(){
 
-        // Inserimento del gli input dell'utente in delle variabili
-        let sconto = 0;
-        let tipoBiglietto = "Biglietto Standard";
-        let carrozzaRandom = (Math.random() * 10) +1 ;
-        const km = Number(document.getElementById('distanza').value);
-        const etaUtente = document.getElementById('eta').value;
-        const nomeUtente = document.getElementById('name').value;
+        // Definizione e assegnazione dei km da percorrere
+        const kmValue= Number(km.value);
 
-        // Stampa a video dei dati inseriti dall'utente come controllo
-        console.log(nomeUtente);
-        console.log({km});
-        console.log({etaUtente});
+        // Controllo se tutti i campi sono stati riempiti correttamente
+        if( nomeUtente.value !== '' &&  kmValue > 0 && etaUtente.value !== ''){
+            let sconto = 0;
+            let tipoBiglietto = "Biglietto Standard";
+            let carrozzaRandom = (Math.random() * 10) +1 ;
+        
+            // Stampa a video dei dati inseriti dall'utente come controllo
+            console.log(nomeUtente.value);
+            console.log('KM: ' + kmValue);
+            console.log('Eta: ' + etaUtente.value);
 
-        // Calcolo e stampa a video del prezzo pieno del biglietto
-        let prezzoBiglietto = euroKm * km;
-        console.log(`${prezzoBiglietto.toFixed(2)}€`);
+            // Calcolo e stampa a video del prezzo pieno del biglietto
+            let prezzoBiglietto = euroKm * kmValue;
+            console.log(`Prezzo: ${prezzoBiglietto.toFixed(2)}€`);
 
-        // Controllo se l'utente in base all'eta scelta se è idoneo ad uno sconto
-        if(etaUtente === 'm'){
-            prezzoBiglietto *= 0.8;
-            sconto = 20;
-            tipoBiglietto = "Biglietto Young";
-        }else if(etaUtente  === 'V'){
-            prezzoBiglietto *= 0.6;
-            sconto = 40;
-            tipoBiglietto = "Biglietto Vecio";
-        }
+            // Controllo se l'utente in base all'eta scelta se è idoneo ad uno sconto
+            if(etaUtente.value === 'm'){
+                prezzoBiglietto *= 0.8;
+                sconto = 20;
+                tipoBiglietto = "Biglietto Young";
+            }else if(etaUtente.value  === 'V'){
+                prezzoBiglietto *= 0.6;
+                sconto = 40;
+                tipoBiglietto = "Biglietto Vecio";
+            }
 
-        // Stampa a video del prezzo finale
-        console.log(`Prezzo Scontato del ${sconto}%: ${prezzoBiglietto.toFixed(2)}€`);
+            // Stampa a video del prezzo finale
+            console.log(`Prezzo Scontato del ${sconto}%: ${prezzoBiglietto.toFixed(2)}€`);
 
-        // Comparsa del biglietto con le informazioni inserite dall'utente
-        bigliettoShow.classList.add('show');
-        bigliettoShow.classList.remove('hidden');
-        document.querySelector('.name').innerHTML = nomeUtente;
-        document.getElementById('tipo-biglietto').innerHTML = tipoBiglietto;
-        document.getElementById('carrozza').innerHTML = Math.floor(carrozzaRandom);
-        document.getElementById('prezzo').innerHTML = prezzoBiglietto.toFixed(2);
+            // Comparsa del biglietto con le informazioni inserite dall'utente
+            bigliettoShow.classList.add('show');
+            
+            document.querySelector('.name').innerHTML = nomeUtente.value;
+            document.getElementById('tipo-biglietto').innerHTML = tipoBiglietto;
+            document.getElementById('carrozza').innerHTML = Math.floor(carrozzaRandom);
+            document.getElementById('prezzo').innerHTML = prezzoBiglietto.toFixed(2);
+        }else {
+            // Output se tutti i campi non sono stati inseriti correttamente
+            alert('Perfavore riempire tutti i campi in modo corretto');
+            console.log('Dati Inseriti non corretti');
+        }   
     }
 );
 
@@ -59,9 +72,9 @@ btnAnnulla.addEventListener(
     'click',
     function(){
         bigliettoShow.classList.remove('show');
-        bigliettoShow.classList.add('hidden');
     }
 )
+
 
 
 
